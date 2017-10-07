cd32f1488a50e0ba504fc1ea4f19289b1c19993d
PolarBearITS <polar.bear.in.the.snow@gmail.com>
2017-10-07 19:08:15 -0400

    new exercises

--- 002.py
+++ 002.py
@@ -1,7 +1,7 @@
 memo = {}
 def fib(n):
 	if n in memo: return memo[n]
-	if n <= 2: f = 1
+	if n <= 1: f = 1
 	else: f = fib(n-1) + fib(n-2)
 	memo[n] = f
 	return f
--- 031.py
+++ 031.py
@@ -1,7 +1,12 @@
 
 def coin_sum(n):
-	values = [1, 2, 5, 10, 20, 50, 100, 200]
-	
-
-sums = {}
-print(coin_sum(2))
+	coins = [1, 2, 5, 10, 20, 50, 100, 200]
+	sums = {k:v for k, v in zip(range(1, n+1), [0]*n)}
+	for coin in coins:
+		for value in sums:
+			if value == coin:
+				sums[value] += 1
+			elif value > coin:
+				sums[value] += sums[value-coin]
+	print(sums[n])
+coin_sum(200)
